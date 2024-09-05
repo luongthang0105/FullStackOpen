c4ef3b56adb90f6eeefa166dd9447cc3a9cd7dfb
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-09-05 22:52:13 +1000

    finished part1 theory

--- Part1/part1/src/App.jsx
+++ Part1/part1/src/App.jsx
@@ -1,10 +1,38 @@
-const App = () => {
-    const friends = [ 'Peter', 'Maya']
-  
-    return (
-      <div>
-        <p>{friends}</p>
-      </div>
-    )
+import { useState } from "react"
+
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return <div>the app is used by pressing the buttons</div>
   }
+  return <div>button press history: {props.allClicks.join(" ")}</div>
+}
+
+const Button = ({ onClick, text }) => {
+  return <button onClick={onClick}>{text}</button>
+}
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const handleLeftClick = () => {
+    setAll(allClicks.concat("L"))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat("R"))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text={"left"}></Button>
+      <Button onClick={handleRightClick} text={"right"}></Button>
+      {right}
+      <History allClicks={allClicks} />
+    </div>
+  )
+}
+
 export default App
