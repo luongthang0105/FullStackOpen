bc3b8b460a891f2a7a2e3053261338c1525c9c60
Luong Thang Nguyen <z5402969@ad.unsw.edu.au>
2024-09-05 23:54:51 +1000

    implement voting

--- Part1/exercises/anecdotes/src/App.jsx
+++ Part1/exercises/anecdotes/src/App.jsx
@@ -1,33 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ]
+  const [points, setPoints] = useState({})
+  const [selected, setSelected] = useState(0)
+
+  console.log(points)
+  const findNumVotes = () => {
+    return Object.hasOwn(points, selected) ? points[selected] : 0
+  }
+
+  const handleVoting = () => {
+    const newPoints = {
+      ...points,
+    }
+    if (Object.hasOwn(newPoints, selected)) {
+      newPoints[selected] += 1
+    } else {
+      newPoints[selected] = 1
+    }
+    setPoints(newPoints)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>{anecdotes[selected]}</div>
+      <div>has {findNumVotes()} votes</div>
+      <button onClick={handleVoting}>vote</button>
+      <button
+        onClick={() =>
+          setSelected(Math.floor(Math.random() * anecdotes.length))
+        }
+      >
+        next anecdote
+      </button>
     </>
   )
 }
